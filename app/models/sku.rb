bac1c8688483de496a81984a40b6b37e3c0f2ef5
wrmk <elmakan@gmail.com>
2021-09-05 18:34:45 +0300

    fix association

--- app/models/sku.rb
+++ app/models/sku.rb
@@ -1,10 +1,11 @@
 class Sku < ApplicationRecord
   self.primary_key = "SKU"
 
-  belongs_to :supplier, :foreign_key => "Код_поставщика"
-
   validates "SKU", presence: true, uniqueness: true
   validates "Код_поставщика", presence: true
   validates "Название_товара", presence: true
   validates "Цена", presence: true
+  
+  belongs_to :supplier, :foreign_key => "Код_поставщика"
+  accepts_nested_attributes_for :supplier  
 end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,7 +1,9 @@
 class Supplier < ApplicationRecord
   self.primary_key = "Код_поставщика"
-  has_many :skus
+  
 
   validates "Код_поставщика", presence: true, uniqueness: true
   validates "Название_поставщика", presence: true
+  has_many :skus, :foreign_key => "Код_поставщика"
+  accepts_nested_attributes_for :skus
 end
