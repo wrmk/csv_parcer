67381e9887531d44dd60b8c1bbbd5d8fe943b929
wrmk <elmakan@gmail.com>
2021-09-15 21:03:09 +0300

    rspec test save and update supplier

---
+++ spec/models/csv_parse_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe CsvParse, type: :model do
+  context 'after parse' do
+    db = 'Supplier'.constantize
+
+    it 'save row if doesn`t exists' do
+      row_hash = {'Код_поставщика' => '15',
+                  'Название_поставщика' => 'John and sons'
+      }
+      
+      check(row_hash,db)
+    end
+    it 'update row of exists' do
+      row_hash = {'Код_поставщика' => '15',
+                  'Название_поставщика' => 'Sons'
+      }
+      check(row_hash,db)
+    end
+  end  
+end
+
+def check(row_hash,db)
+  CsvParse.save(row_hash,db)
+  name = Supplier.find(row_hash['Код_поставщика']).Название_поставщика
+  expect(name).to eql(row_hash['Название_поставщика'])
+end
+
