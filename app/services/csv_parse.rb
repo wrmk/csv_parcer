2dfb3aab5a4337fbddcf983f54aa9544a35da2fb
wrmk <elmakan@gmail.com>
2021-09-15 20:09:56 +0300

    refactoring

--- app/services/csv_parse.rb
+++ app/services/csv_parse.rb
@@ -1,17 +1,21 @@
 class CsvParse
   require 'csv'
 
-  def self.save(columns,file_name,db)
+  def self.parse(columns,file_name,db)
     path = File.join Rails.root, file_name
     CSV.foreach(path, col_sep: '¦') do |row|
       row_hash = Hash[columns.zip(row)]
-      item = db.where([row_hash.first].to_h)
-      if item.exists?
-        item.update(row_hash)
-      else
-        item = db.create(row_hash)
-      end
-      p item     
+      save(row_hash,db)
     end
+  end
+
+  def self.save(row_hash,db)
+    item = db.where([row_hash.first].to_h)
+    if item.exists?
+      item.update(row_hash)
+    else
+      item = db.create(row_hash)
+    end
+    p item 
   end
 end
